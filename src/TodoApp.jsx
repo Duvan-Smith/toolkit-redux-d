badc70dc4a78d9aa54dbcd1bca5f524001860ccf
DsmithMr <dsmith.mr@gmail.com>
2022-07-10 17:46:30 -0500

    Added example

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,14 +1,27 @@
-import { useGetTodosQuery } from "./store/apis";
+import { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis";
 
 export const TodoApp = () => {
-  const { data: todos = [], isLoading } = useGetTodosQuery();
+  const [todoId, setTodoId] = useState(1);
+
+  const { data: todos = [], isLoading: isLoadingList } = useGetTodosQuery();
+  const { data: todo } = useGetTodoQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId(todoId + 1);
+  };
+  const prevTodo = () => {
+    if (todoId === 1) return;
+    setTodoId(todoId - 1);
+  };
 
   return (
     <>
       <h1>Todos - RTK Query</h1>
       <hr />
-      <h4>isLoading: {isLoading ? "true" : "false"}</h4>
-      <pre>...</pre>
+      <h4>isLoading: {isLoadingList ? "true" : "false"}</h4>
+
+      <pre> {JSON.stringify(todo)} </pre>
 
       <ul>
         {todos.map((todo) => (
@@ -16,7 +29,8 @@ export const TodoApp = () => {
         ))}
       </ul>
 
-      <button>Next Todo</button>
+      <button onClick={prevTodo}>Prev Todo</button>
+      <button onClick={nextTodo}>Next Todo</button>
     </>
   );
 };
