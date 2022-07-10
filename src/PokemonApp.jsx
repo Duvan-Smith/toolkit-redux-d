bd4af5a69b58e8c63771f8e56f296dafd6bd9cb0
DsmithMr <dsmith.mr@gmail.com>
2022-07-10 17:01:20 -0500

    Added useSelector to PokemonApp

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getPokemons } from "./store/slices/pokemon";
 
 export const PokemonApp = () => {
   const dispath = useDispatch();
+
+  const { page, pokemons, isLoading } = useSelector((state) => state.pokemons);
 
   useEffect(() => {
     dispath(getPokemons());
@@ -13,9 +15,19 @@ export const PokemonApp = () => {
     <>
       <h1>PokemonApp</h1>
       <hr />
+      <span>Loading: {isLoading ? "True" : "False"}</span>
       <ul>
+        {pokemons.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
         <li>Hola</li>
       </ul>
+      <button
+        disabled={isLoading}
+        onClick={() => dispath(getPokemons(page + 1))}
+      >
+        Next
+      </button>
     </>
   );
 };
